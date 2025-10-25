4ce2f1967653c05ad608c246d78e9757c4d36ce0
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-10-25 18:30:32 +0300

    work

---
+++ src/App.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  text-align: center;
+  font-family: sans-serif;
+  padding: 20px;
+`;
+
+export const Title = styled.h1`
+  color: #333;
+`;
---
+++ src/Choice.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ChoiceContainer = styled.div`
+  margin: 20px 0;
+`;
+
+export const StickerImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+`;
+
+export const Label = styled.p`
+  font-size: 18px;
+  color: #555;
+`;
